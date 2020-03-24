c19b07707380972486e49178fa53539aab99ea49
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2020-03-24 09:31:51 +0100

    Add birthdays

--- src/cron-jobs/birthdays.ts
+++ src/cron-jobs/birthdays.ts
@@ -7,6 +7,58 @@ const birthdays: Birthday[] = [{
   slackId: 'WT7GV5VP1',
   name: 'Sigurd',
   date: '20.04'
+}, {
+  slackId: 'WT8UT7WNB',
+  name: 'Hassan',
+  date: '06.10'
+}, {
+  slackId: 'WSTNZA04T',
+  name: 'Rasmus',
+  date: '26.12'
+}, {
+  slackId: 'WT2GGFBT2',
+  name: 'Asaad',
+  date: '30.11'
+}, {
+  slackId: 'WTK5A1XPV',
+  name: 'Morten',
+  date: '05.03'
+}, {
+  slackId: 'WUBFMTCCE',
+  name: 'Roger',
+  date: '30.05'
+}, {
+  slackId: 'WT57JJN3X',
+  name: 'Varun',
+  date: '23.05'
+}, {
+  slackId: 'WT70SFZ5M',
+  name: 'Alexander',
+  date: '22.01'
+}, {
+  slackId: 'WT7393A6T',
+  name: 'Tom Christian',
+  date: '20.02'
+}, {
+  slackId: 'WT4AA4B71',
+  name: 'Magnus',
+  date: '15.03'
+}, {
+  slackId: 'WT51772AU',
+  name: 'Øyvind',
+  date: '28.11'
+}, {
+  slackId: 'WSTNZ9KJ7',
+  name: 'Ritesh',
+  date: '27.02'
+}, {
+  slackId: 'WV1QRNHUZ',
+  name: 'Eddy',
+  date: '11.10'
+}, {
+  slackId: 'WUPDHPDQR',
+  name: 'Brage',
+  date: '12.07'
 }];
 
 const getUserGroupMembers = async () =>
